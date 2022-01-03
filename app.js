65c8422fa048279acc005f50de7f9e31d6be05b9
tthanhvu002 <81304749+tthanhvu002@users.noreply.github.com>
2022-01-03 20:26:50 +0700

    pj

---
+++ app.js
@@ -0,0 +1,22 @@
+const moreBtns = document.querySelectorAll('.more')
+const popups = document.querySelectorAll('.popup')
+Array.from(moreBtns).map((btn,index) => {
+    btn.onclick = () => {
+        popups[index].classList.toggle('active')
+    }
+})
+
+const visibleBtn = document.querySelector('.visible-btn')
+visibleBtn.onclick = () => {
+    document.querySelector('.overflow').classList.toggle('active')
+}
+
+const mobileMenuBtn = document.querySelector('.mobile-menu__icon')
+mobileMenuBtn.onclick = () => {
+    document.querySelector('.mobile-overflow').classList.toggle('active')
+    mobileMenuBtn.classList.toggle('active')
+    console.log(1);
+}
+document.querySelector('.header__mobile-menu-icon').onclick = () => {
+    document.querySelector('.header__mobile-menu').classList.toggle('active')
+}
